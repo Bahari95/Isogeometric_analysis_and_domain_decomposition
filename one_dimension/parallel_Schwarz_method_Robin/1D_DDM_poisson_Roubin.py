72ec4762ef1f75c9316d6225e7f775a734d54eb6
Bahari Mustapha <75118127+Bahari95@users.noreply.github.com>
2023-05-18 15:18:42 +0200

    Add files via upload

---
+++ one_dimension/parallel_Schwarz_method_Robin/1D_DDM_poisson_Roubin.py
@@ -0,0 +1,155 @@
+from simplines import compile_kernel, apply_dirichlet
+
+from simplines import SplineSpace
+from simplines import TensorSpace
+from simplines import StencilMatrix
+from simplines import StencilVector
+from simplines import pyccel_sol_field_2d
+
+# .. Matrices in 1D ..
+from gallery_section_04 import assemble_stiffnessmatrix1D
+assemble_stiffness1D = compile_kernel( assemble_stiffnessmatrix1D, arity=2)
+#---In Poisson equation
+from gallery_section_04 import assemble_vector_ex01   
+from gallery_section_04 import assemble_norm_ex01     
+
+assemble_rhs         = compile_kernel(assemble_vector_ex01, arity=1)
+assemble_norm_l2     = compile_kernel(assemble_norm_ex01, arity=1)
+
+#from matplotlib.pyplot import plot, show
+import matplotlib.pyplot            as     plt
+from   mpl_toolkits.axes_grid1      import make_axes_locatable
+from   mpl_toolkits.mplot3d         import axes3d
+from   matplotlib                   import cm
+from   mpl_toolkits.mplot3d.axes3d  import get_test_data
+from   matplotlib.ticker            import LinearLocator, FormatStrFormatter
+#..
+from   scipy.sparse                 import kron
+from   scipy.sparse                 import csr_matrix
+from   scipy.sparse                 import csc_matrix, linalg as sla
+from   numpy                        import zeros, linalg, asarray, linspace
+from   numpy                        import cos, sin, pi, exp, sqrt, arctan2
+from   tabulate                     import tabulate
+import numpy                        as     np
+import timeit
+import time
+
+#==============================================================================
+#.......Poisson ALGORITHM
+class DDM_poisson(object):
+
+   def __init__(self, V, V2, S_DDM, domain_nb, ovlp_value):
+       # ++++
+       #... We delete the first and the last spline function
+       #. as a technic for applying Dirichlet boundary condition
+
+       #..Stiffness and Mass matrix in 1D in the first deriction
+       K                  = assemble_stiffness1D(V)
+       K                  = K.tosparse()
+       if domain_nb == 0 :
+          K               = K.toarray()[1:,1:]
+          K[-1,-1]       += S_DDM
+       else :
+          K               = K.toarray()[:-1,:-1]
+          K[0,0]         += S_DDM
+       K                  = csr_matrix(K)
+
+       # ...
+       self.lu             = sla.splu(csc_matrix(K))
+       
+       # ++++
+       self.spaces        = [V, V2]
+       self.ovlp_value    = ovlp_value
+       self.S_DDM         = S_DDM
+       self.domain_nb     = domain_nb
+   def solve(self, u_d):
+
+       V, Vh               = self.spaces[:]
+
+       #--Assembles a right hand side of Poisson equation
+       rhs                 = StencilVector(V.vector_space)
+       rhs                 = assemble_rhs( Vh, fields = [u_d], knots = True, value = [self.ovlp_value, self.S_DDM, self.domain_nb], out = rhs )
+       b                   = rhs.toarray()
+       if self.domain_nb == 0 :
+          b                   = b[1:]      
+       else :
+          b                   = b[:-1] 
+
+       xkron               = self.lu.solve(b)       
+       # ...
+       x                   = np.zeros(V.nbasis)
+       if self.domain_nb == 0 :
+          x[1:]  = xkron[:]
+       else :
+          x[:-1] = xkron[:]
+       #...
+       u  = StencilVector(V.vector_space)
+       u.from_array(V, x)
+       # ...
+       Norm                = assemble_norm_l2(V, fields=[u]) 
+       norm                = Norm.toarray()
+       l2_norm             = norm[0]
+       H1_norm             = norm[1]       
+       return u, x, l2_norm, H1_norm
+
+degree      = 2
+quad_degree = degree + 1
+nelements   = 1024
+
+# ... please take into account that : beta < alpha 
+alpha       = 0.5
+beta        = 0.5
+overlap     = alpha - beta
+xuh_0       = []
+xuh_01      = []
+iter_max    = 10
+S_DDM       = 1./(beta)
+#--------------------------
+#..... Initialisation
+#--------------------------
+grids_0 = linspace(0, alpha, nelements+1)
+# create the spline space for each direction
+V_0    = SplineSpace(degree=degree, nelements= nelements, grid =grids_0, nderiv = 2, quad_degree = quad_degree)
+
+grids_1 = linspace(beta, 1., nelements+1)
+# create the spline space for each direction
+V_1    = SplineSpace(degree=degree, nelements= nelements, grid =grids_1, nderiv = 2, quad_degree = quad_degree)
+
+Vt_0    = TensorSpace(V_0, V_1)
+Vt_1    = TensorSpace(V_1, V_0)
+
+DDM_0 = DDM_poisson( V_0, Vt_0,  S_DDM, 0, alpha )
+DDM_1 = DDM_poisson( V_1, Vt_1,  S_DDM, 1, beta )
+
+# ... communication Dirichlet interface
+u_00    = StencilVector(V_0.vector_space)
+u_1     = StencilVector(V_1.vector_space)
+
+print('#---IN-UNIFORM--MESH')
+u_0,   xuh, l2_norm, H1_norm     = DDM_0.solve( u_1)
+xuh_0.append(xuh)
+u_1, xuh_1, l2_norm1, H1_norm1   = DDM_1.solve( u_00)
+xuh_01.append(xuh_1)
+u_00 = u_0
+l2_err = l2_norm + l2_norm1
+H1_err = H1_norm + H1_norm1
+print('-----> L^2-error ={} -----> H^1-error = {}'.format(l2_err, H1_err))
+
+for i in range(iter_max):
+	#...
+	u_0, xuh, l2_norm, H1_norm     = DDM_0.solve(u_1)
+	xuh_0.append(xuh)
+	u_1, xuh_1, l2_norm1, H1_norm1 = DDM_1.solve(u_00)
+	xuh_01.append(xuh_1)
+	u_00   = u_0
+	l2_err = l2_norm + l2_norm1
+	H1_err = H1_norm + H1_norm1
+	print('-----> L^2-error ={} -----> H^1-error = {}'.format(l2_err, H1_err))
+
+#---Compute a solution
+from simplines import plot_field_1d
+nbpts = 100
+plt.figure()
+plot_field_1d(V_0.knots, V_0.degree, xuh, nx=101, color='b')
+plot_field_1d(V_1.knots, V_1.degree, xuh_1, nx=101, color='r')
+plt.show()
---
+++ one_dimension/parallel_Schwarz_method_Robin/gallery_section_04.py
@@ -0,0 +1,269 @@
+__all__ = ['assemble_matrix_ex01',
+           'assemble_vector_ex01',
+           'assemble_norm_ex01',
+           'assemble_matrix_ex02',
+           'assemble_vector_ex02',
+           'assemble_norm_ex02'
+]
+
+from pyccel.decorators import types
+
+# assembles stiffness matrix 1D
+#==============================================================================
+@types('int', 'int', 'int[:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]')
+def assemble_stiffnessmatrix1D(ne, degree, spans, basis, weights, points,  matrix):
+
+    # ... sizes
+    k1 = weights.shape[1]
+    # ... build matrices
+    for ie1 in range(0, ne):
+            i_span_1 = spans[ie1]        
+            # evaluation dependant uniquement de l'element
+
+            for il_1 in range(0, degree+1):
+                i1 = i_span_1 - degree + il_1
+                for il_2 in range(0, degree+1):
+                            i2 = i_span_1 - degree + il_2
+                            v = 0.0
+                            for g1 in range(0, k1):
+                                
+                                    bi_x = basis[ie1, il_1, 1, g1]
+                                    bj_x = basis[ie1, il_2, 1, g1]
+                                    
+                                    wvol = weights[ie1, g1]
+                                    
+                                    v += bi_x * bj_x * wvol
+
+                            matrix[ degree+ i1, degree+ i2-i1]  += v
+
+#==============================================================================Assemble rhs Poisson
+#---1 : In uniform mesh
+@types('int', 'int', 'int', 'int', 'int[:]', 'int[:]', 'double[:,:,:,:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'real[:]', 'real[:]', 'double[:]', 'real', 'real', 'int', 'double[:]')
+def assemble_vector_ex01(ne1, ne2, p1, p2, spans_1, spans_2, basis_1, basis_2, weights_1, weights_2, points_1, points_2, knots_1, knots_2, vector_d, ovlp_value, S_DDM, domain_nb, rhs):
+
+    from numpy import exp
+    from numpy import pi
+    from numpy import sin
+    from numpy import arctan2
+    from numpy import cos, cosh
+    from numpy import sqrt
+    from numpy import zeros 
+    from numpy import empty
+
+    # ... sizes
+    k1        = weights_1.shape[1]
+
+    # ... build rhs
+    for ie1 in range(0, ne1):
+            i_span_1 = spans_1[ie1]
+            for il_1 in range(0, p1+1):
+                    i1 = i_span_1 - p1 + il_1
+
+                    v = 0.0
+                    for g1 in range(0, k1):
+                            
+                            bi_0 = basis_1[ie1, il_1, 0, g1]
+
+                            wvol  = weights_1[ie1, g1]
+                            x  = points_1[ie1, g1]
+                            # .. Test 0
+                            u    = pi**2*sin(pi*x)
+                            # ... test 1                                 
+                            #u  = -(15625.0 - 31250.0*x)*(2*x - 1.0)*exp(-7812.5*((x - 0.5)**2 + (y - 0.5)**2 - 0.096)**2) 
+                            #u += 7812.5*(15625.0 - 31250.0*x)*(4*x - 2.0)*((x - 0.5)**2 + (y - 0.5)**2 - 0.096)**2*exp(-7812.5*((x - 0.5)**2 + (y - 0.5)**2 - 0.096)**2) 
+                            #u += -(15625.0 - 31250.0*y)*(2*y - 1.0)*exp(-7812.5*((x - 0.5)**2 + (y - 0.5)**2 - 0.096)**2) 
+                            #u += 7812.5*(15625.0 - 31250.0*y)*(4*y - 2.0)*((x - 0.5)**2 + (y - 0.5)**2 - 0.096)**2*exp(-7812.5*((x - 0.5)**2 + (y - 0.5)**2 - 0.096)**2) 
+                            #u += -2*(-31250.0*(x - 0.5)**2 - 31250.0*(y - 0.5)**2 + 3000.0)*exp(-7812.5*((x - 0.5)**2 + (y - 0.5)**2 - 0.096)**2)
+                            # ..
+                            v += bi_0 * u * wvol
+
+                    rhs[i1+p1] += v  
+
+    # ...
+    lcoeffs_d   = zeros(p1+1)
+    #---Computes All basis in at the interface of the overllape points
+    nders          = 1
+    degree         = p2
+    #..
+    left           = empty( degree )
+    right          = empty( degree )
+    a              = empty( (       2, degree+1) )
+    ndu            = empty( (degree+1, degree+1) )
+    ders           = zeros( (     nders+1, degree+1) ) # output array
+    #...
+    basis2         = zeros((nders+1, degree+1))
+    xq = ovlp_value
+    #~~~~~~~~~~~~~~~
+    low  = degree
+    high = len(knots_2)-1-degree
+    # Check if point is exactly on left/right boundary, or outside domain
+    if xq <= knots_2[low ]: 
+         span = low
+    if xq >= knots_2[high]: 
+         span = high-1
+    else :
+      # Perform binary search
+      span = (low+high)//2
+      while xq < knots_2[span] or xq >= knots_2[span+1]:
+         if xq < knots_2[span]:
+             high = span
+         else:
+             low  = span
+         span = (low+high)//2
+    ndu[0,0] = 1.0
+    for j in range(0,degree):
+        left [j] = xq - knots_2[span-j]
+        right[j] = knots_2[span+1+j] - xq
+        saved    = 0.0
+        for r in range(0,j+1):
+            # compute inverse of knot differences and save them into lower triangular part of ndu
+            ndu[j+1,r] = 1.0 / (right[r] + left[j-r])
+            # compute basis functions and save them into upper triangular part of ndu
+            temp       = ndu[r,j] * ndu[j+1,r]
+            ndu[r,j+1] = saved + right[r] * temp
+            saved      = left[j-r] * temp
+        ndu[j+1,j+1] = saved	
+
+    # Compute derivatives in 2D output array 'ders'
+    ders[0,:] = ndu[:,degree]
+    for r in range(0,degree+1):
+        s1 = 0
+        s2 = 1
+        a[0,0] = 1.0
+        for k in range(1,nders+1):
+            d  = 0.0
+            rk = r-k
+            pk = degree-k
+            if r >= k:
+               a[s2,0] = a[s1,0] * ndu[pk+1,rk]
+               d = a[s2,0] * ndu[rk,pk]
+            j1 = 1   if (rk  > -1 ) else -rk
+            j2 = k-1 if (r-1 <= pk) else degree-r
+            for ij in range(j1,j2+1):
+                a[s2,ij] = (a[s1,ij] - a[s1,ij-1]) * ndu[pk+1,rk+ij]
+            for ij in range(j1,j2+1):
+                d += a[s2,ij]* ndu[rk+ij,pk]
+            if r <= pk:
+               a[s2,k] = - a[s1,k-1] * ndu[pk+1,r]
+               d += a[s2,k] * ndu[r,pk]
+            ders[k,r] = d
+            j  = s1
+            s1 = s2
+            s2 = j
+    # Multiply derivatives by correct factors
+    r = degree
+    ders[1,:] = ders[1,:] * r
+    basis2[0,:] = ders[0,:]
+    basis2[1,:] = ders[1,:]
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Assembles Neumann Condition
+    if domain_nb == 0 :
+      i1_ovrlp  = ne1+2*p1-1
+      neum_sign = 1.
+    else :
+      i1_ovrlp  = p1
+      neum_sign = -1.
+                
+    #... We compute firstly the span in new adapted points                             
+    #...                    
+    xq        = ovlp_value
+    degree    = p2
+    low       = degree
+    high      = len(knots_2)-1-degree
+    # Check if point is exactly on left/right boundary, or outside domain
+    if xq <= knots_2[low ]: 
+         span = low
+    if xq >= knots_2[high]: 
+         span = high-1
+    else :
+      # Perform binary search
+      span = (low+high)//2
+      while xq < knots_2[span] or xq >= knots_2[span+1]:
+         if xq < knots_2[span]:
+             high = span
+         else:
+             low  = span
+         span = (low+high)//2
+    span_2    = span 
+    # ...
+    lcoeffs_d[:] = vector_d[span_2 : span_2+p2+1]
+    # ...
+    u   = 0.
+    ux  = 0.
+    for il_1 in range(0, p2+1):
+             bi_0      = basis2[0,il_1]
+             bi_x      = basis2[1,il_1]
+             # ...
+             coeff_d   = lcoeffs_d[il_1]
+             # ...
+             u        +=  coeff_d*bi_0
+             ux       +=  coeff_d*bi_x
+    # ...
+    rhs[i1_ovrlp] += neum_sign*ux+S_DDM*u
+    # ...
+
+#==============================================================================Assemble l2 and H1 error norm
+#---1 : In uniform mesh
+@types('int', 'int', 'int[:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:]', 'double[:]')
+def assemble_norm_ex01(ne1, p1, spans_1, basis_1, weights_1, points_1, vector_u, rhs):
+
+    from numpy import exp
+    from numpy import cos
+    from numpy import sin
+    from numpy import pi
+    from numpy import sqrt
+    from numpy import zeros
+
+    # ... sizes
+    k1 = weights_1.shape[1]
+    # ...
+
+    lcoeffs_u  = zeros(p1+1)
+    lvalues_u  = zeros(k1)
+    lvalues_ux = zeros(k1)
+
+    norm_l2 = 0.
+    norm_H1 = 0.
+    # ...
+    for ie1 in range(0, ne1):
+            i_span_1 = spans_1[ie1]
+
+            lvalues_u[ : ]  = 0.0
+            lvalues_ux[ : ] = 0.0
+            lcoeffs_u[ : ]  = vector_u[i_span_1 : i_span_1+p1+1]
+            for il_1 in range(0, p1+1):
+                    coeff_u = lcoeffs_u[il_1]
+
+                    for g1 in range(0, k1):
+                            b1  = basis_1[ie1,il_1,0,g1]
+                            db1 = basis_1[ie1,il_1,1,g1]
+
+                            lvalues_u[g1]  += coeff_u * b1
+                            lvalues_ux[g1] += coeff_u * db1
+
+            v = 0.0
+            w = 0.0
+            for g1 in range(0, k1):
+                    wvol = weights_1[ie1, g1]
+
+                    x    = points_1[ie1, g1]
+
+                    # ... test 0
+                    u   = sin(pi*x)
+                    ux  = pi*cos(pi*x)
+                    # ... test 1
+                    
+                    uh  = lvalues_u[g1]
+                    uhx = lvalues_ux[g1]
+
+                    v  += (u-uh)**2 * wvol
+                    w  += ((ux-uhx)**2) * wvol
+            norm_l2 += v
+            norm_H1 += w
+
+    norm_l2 = sqrt(norm_l2)
+    norm_H1 = sqrt(norm_H1)
+
+    rhs[p1]   = norm_l2
+    rhs[p1+1] = norm_H1
+    # ...
